63920b88c44bad69cfa5c7615c90586195718b32
pierre <pierre.depaz@gmail.com>
2018-01-05 01:16:21 +0100

    initiated

---
+++ main.js
@@ -0,0 +1,26 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+const path = require('path')
+const url = require('url')
+
+let mainWindow
+
+createWindow = () => {
+	mainWindow = new BrowserWindow({width: 800, height: 800})
+
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'app/index.html'),
+		protocol: 'file',
+		slashes: true
+	}))
+
+	mainWindow.on('closed', () => {
+		mainWindow = null
+	})
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => { app.quit() })
