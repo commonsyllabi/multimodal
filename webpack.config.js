3f9480914e4c9a199dda415c344cd03d24689470
pierre <pierre.depaz@gmail.com>
2018-01-08 12:42:44 +0100

    added sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack')
 const electron = require('electron')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
   entry: './src/renderer/index.js',
@@ -8,5 +9,18 @@ module.exports = {
     filename: 'renderer.js',
     path: path.resolve(__dirname, 'app')
   },
+  module: {
+      loaders: [
+          {
+              test: /\.scss$/,
+              loader: ExtractTextPlugin.extract('css-loader!sass-loader')
+          }
+      ]
+  },
+  plugins: [
+      new ExtractTextPlugin('style.css', {
+          allChunks: true
+      })
+  ],
   target: "electron-main"
 };
