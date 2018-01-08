83776b0bbdb282cf6b068f62a233475d80c8ec3e
pierre <pierre.depaz@gmail.com>
2018-01-09 00:27:04 +0100

    linted

--- src/renderer/mouse.js
+++ src/renderer/mouse.js
@@ -3,21 +3,21 @@
 import { currentNote, setCurrrentPosition } from './globals.js'
 
 let position = {
-  x : 0,
-  y: 0
+	x : 0,
+	y: 0
 }
 
-let getPosition = () =>{;
-  return position
+let getPosition = () =>{
+	return position
 }
 
 let handle = (event) => {
-  position.x = event.pageX
-  position.y = event.pageY
+	position.x = event.pageX
+	position.y = event.pageY
 
-  if(currentNote != null){
-    setCurrrentPosition(position)
-  }
+	if(currentNote != null){
+		setCurrrentPosition(position)
+	}
 
 }
 
--- src/renderer/typing.js
+++ src/renderer/typing.js
@@ -10,40 +10,39 @@ const RET = 13
 const TAB = 9
 
 let currentNote = null
-let floating = true
 
 let handle = (e) => {
-  currentNote = getCurrentNote()
+	currentNote = getCurrentNote()
 
-  if(currentNote == null && e.keyCode != SPC){
-    console.log('key pressed',e.key,'with charCode',e.keyCode,'but no note created, ignoring...')
-    return
-  }
+	if(currentNote == null && e.keyCode != SPC){
+		console.log('key pressed',e.key,'with charCode',e.keyCode,'but no note created, ignoring...')
+		return
+	}
 
-  switch (e.keyCode) {
-    case SPC:
-      if(currentNote == null)
-        newNote()
-      else
-        handleKey("\u00A0")
-      break;
-    case BCK:
-      eraseCharacter()
-      break;
-    case ESC:
-      endNote()
-      break;
-    case TAB:
-      handleKey("\u00A0\u00A0\u00A0\u00A0")
-      break;
-    case RET:
-      handleKey("\n")
-      break;
-    default:
-      if(e.keyCode > 47)
-        handleKey(e.key)
-      break;
-  }
+	switch (e.keyCode) {
+	case SPC:
+		if(currentNote == null)
+			newNote()
+		else
+			handleKey('\u00A0')
+		break
+	case BCK:
+		eraseCharacter()
+		break
+	case ESC:
+		endNote()
+		break
+	case TAB:
+		handleKey('\u00A0\u00A0\u00A0\u00A0')
+		break
+	case RET:
+		handleKey('\n')
+		break
+	default:
+		if(e.keyCode > 47)
+			handleKey(e.key)
+		break
+	}
 
 
 }
@@ -52,27 +51,27 @@ let newNote = () => {
 	let cn = document.createElement('div')
 	cn.setAttribute('class', 'note')
 	cn.setAttribute('id', 'current')
-  cn.innerText = "_"
+	cn.innerText = '_'
 	document.body.append(cn)
 
-  setCurrentNote(cn)
-  setCurrrentPosition(mouse.getPosition())
+	setCurrentNote(cn)
+	setCurrrentPosition(mouse.getPosition())
 }
 
 let endNote = () => {
 	currentNote.removeAttribute('id')
-  currentNote.innerText = currentNote.innerText.slice(0, -1)
+	currentNote.innerText = currentNote.innerText.slice(0, -1)
 	setCurrentNote(null)
 }
 
 let handleKey = (char) => {
-  if(char == "Meta") return
-  currentNote.innerText = currentNote.innerText.slice(0, -1)
-	currentNote.innerText += char + "_"
+	if(char == 'Meta') return
+	currentNote.innerText = currentNote.innerText.slice(0, -1)
+	currentNote.innerText += char + '_'
 }
 
 let eraseCharacter = () => {
-  currentNote.innerText = currentNote.innerText.slice(0, -2) + "_"
- }
+	currentNote.innerText = currentNote.innerText.slice(0, -2) + '_'
+}
 
 export { handle }
