56ed04a55ecefefdec4634d627a86baf13aaa84c
pierre <pierre.depaz@gmail.com>
2018-01-19 22:34:09 +0400

    added quantized position

--- src/renderer/mouse.js
+++ src/renderer/mouse.js
@@ -7,8 +7,16 @@ let position = {
 	y: 0
 }
 
-let getPosition = () =>{
-	return position
+let getGridPosition = () =>{
+	let normalized_pos = {
+		x : 0,
+		y : 0
+	}
+
+	normalized_pos.x = Math.floor(map(position.x, 0, 1800, 0, 18))*100
+	normalized_pos.y = Math.floor(map(position.y, 0, 1000, 0, 25))*40
+
+	return normalized_pos
 }
 
 let handle = (event) => {
@@ -16,9 +24,13 @@ let handle = (event) => {
 	position.y = event.pageY
 
 	if(currentNote != null){
-		setCurrrentPosition(position)
+		setCurrrentPosition(getGridPosition())
 	}
 
 }
 
-export { getPosition, handle }
+let map = (value, start_1, end_1, start_2, end_2) => {
+	return start_2 + (end_2 - start_2) * (value - start_1) / (end_1 - start_1)
+}
+
+export { getGridPosition, handle }
