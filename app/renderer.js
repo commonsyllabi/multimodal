e2801a4245e3182f60828a397d85354668a39728
pierre <pierre.depaz@gmail.com>
2018-01-21 22:33:11 +0400

    added template rendering

--- app/renderer.js
+++ app/renderer.js
@@ -106,7 +106,6 @@ let getCurrentConcept = () => {
 
 let setCurrrentPosition = (pos) => {
 	currentNote.style.cssText = 'top: '+pos.y+'px; left: '+pos.x+'px;'
-	console.log(currentNote.style.cssText)
 }
 
 
