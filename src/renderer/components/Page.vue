132b8a0e741e223fd4011417eb7acfbf1b4130e6
pierre <pierre.depaz@gmail.com>
2020-01-31 01:15:20 +0100

    changed frontend to one markdown prep

--- src/renderer/components/Page.vue
+++ src/renderer/components/Page.vue
@@ -11,9 +11,10 @@
     </div>
 
     <!-- ALL PREPS -->
-    <Prep v-for="(prep, index) in data.preps" :data="prep" :key="`prep-${index}`" :_id="`prep-${index}`" :subject="subject" :index="index"
+    <!-- <Prep v-for="(prep, index) in data.preps" :data="prep" :key="`prep-${index}`" :_id="`prep-${index}`" :subject="subject" :index="index"
       @remove-prep="removePrep(index)"
-      @add-prep="addPrep" :isEdit="isEdit"/>
+      @add-prep="addPrep" :isEdit="isEdit"/> -->
+    <Prep :data="data.prep" :subject="subject" :isEdit="isEdit"/>
 
     <!-- ALL NOTES -->
     <Note v-for="(note, index) in data.notes" :data="note" :key="`note-${index}`" @new-note="handleNewNote" :isEdit="isEdit"/>
